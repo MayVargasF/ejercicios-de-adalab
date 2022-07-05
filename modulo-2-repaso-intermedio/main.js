8c366b8d8ba1f91955ac2773658cbc57a14d75ee
MayVargasF <maytevargasfelix@gmail.com>
2022-07-05 12:58:40 +0200

    Background change color added

--- modulo-2-repaso-intermedio/main.js
+++ modulo-2-repaso-intermedio/main.js
@@ -1,25 +1,33 @@
 'use strict';
 
 //Elementos del html que necesito: las dos caritas, el selector de mood y el botón.
-//Escuchar el evento click sobre el botón
+//Escuchar el evento click sobre el botón: actualizarSelect() + cambiarFondo()
+
+//actualizarSelect()
 //recoger el valor del imput: good, bad
 //añadir o quitar clase hidden según el valor del imput
-//BONUS: lo del generar el número y todo eso.
+
+//cambiarFondo()
+//Crear una función que genere un número aleatorio
+//Recoger el random, verificar si es par o impar y cambiar el color de fondo
 
 
 const happyFace = document.querySelector('.js_happyFace');
 const sadFace = document.querySelector('.js_sadFace');
 const selectInput = document.querySelector('.js_selector');
 const button = document.querySelector('.js_button');
-
-const generarAleatorio = (min,max) => {
-    return Math.random()
-}
+const main = document.querySelector('.js_main');
 
 
-function handleClick (ev){
-    ev.preventDefault();
-   
+
+//Versión más sencilla de pintar la carita: en realidad sólo había que crear un sólo div de face e imprimir ahí el value que sería la carita en sí.
+
+// function updateMain2 (){
+//     const selectValue = selectInput.value;
+//     face.innerHTML =selectValue
+// }
+
+function updateMain (){
     const value = selectInput.value;
     console.log(value);
 
@@ -31,8 +39,38 @@ function handleClick (ev){
         sadFace.classList.add('hidden');
     }
 
-    generarAleatorio(1,100)
-  
 }
 
-button.addEventListener('click', handleClick);
+//Math es un objeto de js que tiene muchos métodos (return es un método de Math) -- floor (redondea hacia abajo) y ceil (redondea hacia arriba) es un método para redondear.
+
+const getRandom = (max) => {
+    return parseInt(Math.random()*max);
+}
+
+const changeBackground = () => {
+    const randomNumber = getRandom(100);
+
+    console.log(randomNumber);
+
+    if (randomNumber % 2 === 0){
+        //console.log ('es par');
+        main.classList.add('yellow');
+        main.classList.remove('orange');
+    } 
+    else {
+        //console.log ('es impar');
+        main.classList.add('orange');
+        main.classList.remove('yellow');
+    }
+
+    
+}
+
+
+function handleClick (ev){
+    ev.preventDefault();
+    updateMain();
+    changeBackground();
+}
+
+button.addEventListener('click', handleClick); //aquí handleClick es un callback que funciona como parámetro, no la ejecutamos nosotras,es ejecutada por el navegador (función ejecutada por otra función)
